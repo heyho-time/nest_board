47310fd38e496d68cbec4c687cac5134d3fc8b8e
성해호 <heyho9292@gmail.com>
2022-05-08 01:43:51 +0900

    JWT token

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserRepository } from './user.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserRepository])],
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: 'Secret1234', //아무거나 넣어줘도 됨.
+      signOptions: {
+        expiresIn: 60 * 60,
+      },
+    }),
+    TypeOrmModule.forFeature([UserRepository]),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
